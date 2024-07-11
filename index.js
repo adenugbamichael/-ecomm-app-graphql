4dcefe440946a70849d09291de4ff6aa98543994
adenugbamichael <adenugbamicke@gmail.com>
2024-07-11 04:35:14 +0100

    Setting up the graphQL server | Scalar types

---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require("apollo-server")
+
+const typeDefs = gql`
+  type Query {
+    welcome: String!
+    numOfCourses: Int
+    price: Float
+    isTrainer: Boolean
+  }
+`
+const resolvers = {
+  Query: {
+    welcome: () => {
+      return null
+    },
+    numOfCourses: () => {
+      return 12
+    },
+    price: () => {
+      return 1465.98
+    },
+    isTrainer: () => {
+      return true
+    },
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => console.log(`Server is running at ${url}`))
