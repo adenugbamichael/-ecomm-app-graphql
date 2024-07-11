26cc2262101749fba1b963bec9ce05ad54705ead
adenugbamichael <adenugbamicke@gmail.com>
2024-07-11 18:09:01 +0100

    Reorganizing the Code | fix bug

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const { typeDefs } = require("./schema")
 const { Query } = require("./resolvers/Query")
 const { Course } = require("./resolvers/Course")
 const { Genre } = require("./resolvers/Genre")
+const { courses, genres } = require("./database")
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: { Query, Course, Genre },
+  context: { courses, genres },
 })
 server.listen().then(({ url }) => console.log(`Server is running at ${url}`))
